f8b9c4fc6b83e1e6bcca70111f5456a8431091e5
overmind1980 <overmind1980@qq.com>
2016-01-28 14:32:12 +0800

    Initial commit

---
+++ src/org/oeasy/spigot/plugin/MyPlugin.java
@@ -0,0 +1,19 @@
+package org.oeasy.spigot.plugin;
+
+/**
+ * Created by overmind1980 on 2016/1/28.
+ */
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
